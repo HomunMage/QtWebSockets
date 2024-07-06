e74711a34ba60cbd16d9ba43ac7b639d5f01d8d8
LatticeMage <lattice@posetmage.com>
2024-07-06 13:37:38 +0800

    todo render a node

--- server.py
+++ server.py
@@ -1,24 +1,58 @@
-# server.py
+import sys
+from PySide6.QtCore import QObject, QCoreApplication, QTimer, QByteArray
+from PySide6.QtWebSockets import QWebSocketServer, QWebSocket
+from PySide6.QtNetwork import QHostAddress
+from PySide6.QtGui import QPainter, QColor
+from PySide6.QtWidgets import QApplication, QWidget
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+class CanvasWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 200, 200)
+    
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QColor(255, 255, 255))
+        painter.drawRect(0, 0, self.width(), self.height())
 
-app = FastAPI()
+        painter.setBrush(QColor(0, 0, 0))
+        painter.drawRect(50, 50, 100, 100)
+        
+        painter.setPen(QColor(255, 255, 255))
+        painter.drawText(75, 110, "Start")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    html_content = """
-    <html>
-        <head>
-            <title>FastAPI with PySide6</title>
-        </head>
-        <body>
-            <h1>Hello from FastAPI!</h1>
-        </body>
-    </html>
-    """
-    return html_content
+    def get_drawing_command(self):
+        return "draw square 50 50 100 100 Start"
+
+class WebSocketServer(QObject):
+    def __init__(self, widget):
+        super().__init__()
+        self.widget = widget
+        self.server = QWebSocketServer("Qt WebSocket Server", QWebSocketServer.NonSecureMode, self)
+        if not self.server.listen(QHostAddress.Any, 8000):
+            print("Error: Unable to start the server.")
+            sys.exit(-1)
+        self.server.newConnection.connect(self.on_new_connection)
+        self.clients = []
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.send_drawing_command)
+        self.timer.start(1000)  # Send commands every 1000 ms
+
+    def on_new_connection(self):
+        client = self.server.nextPendingConnection()
+        client.disconnected.connect(lambda: self.on_client_disconnected(client))
+        self.clients.append(client)
+
+    def on_client_disconnected(self, client):
+        self.clients.remove(client)
+
+    def send_drawing_command(self):
+        command = self.widget.get_drawing_command()
+        for client in self.clients:
+            client.sendTextMessage(command)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    app = QApplication(sys.argv)
+    widget = CanvasWidget()
+    server = WebSocketServer(widget)
+    sys.exit(app.exec())
