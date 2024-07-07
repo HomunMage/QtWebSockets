f05b518f4bf714c28216b401de18c352f418df71
HomunMage <homun@posetmage.com>
2024-07-07 10:20:58 +0800

    still cannot find bug

--- CustomGraphicsView.py
+++ CustomGraphicsView.py
@@ -2,33 +2,30 @@
 
 from PySide6.QtWidgets import QGraphicsView, QMenu
 from PySide6.QtGui import QAction
-from PySide6.QtCore import Qt, QPointF
+from PySide6.QtCore import QPointF
 from Node import Node
 
 class CustomGraphicsView(QGraphicsView):
     def __init__(self, scene, widget):
         super().__init__(scene)
         self.setDragMode(QGraphicsView.NoDrag)
-        self._dragging = False
-        self._last_mouse_pos = QPointF()
         self.widget = widget
 
-    def mousePressEvent(self, event):
+    def mouseReleaseEvent(self, event):
         if event.button() == Qt.RightButton:
-            self._last_mouse_pos = event.pos()
-            item = self.itemAt(event.pos())
-            self.show_context_menu(event.pos(), item)
+            self.show_context_menu(event.pos())
         else:
-            super().mousePressEvent(event)
+            super().mouseReleaseEvent(event)
 
-    def show_context_menu(self, position, item):
+    def show_context_menu(self, position):
+        self.right_click_position = self.mapToScene(position)
         context_menu = QMenu(self)
-        scene_position = self.mapToScene(position)
 
-        add_action = QAction("Add Node", self)
-        add_action.triggered.connect(lambda: self.add_node(scene_position))
-        context_menu.addAction(add_action)
+        add_node_action = QAction("Add Node", self)
+        add_node_action.triggered.connect(lambda: self.add_node(self.right_click_position))
+        context_menu.addAction(add_node_action)
 
+        item = self.itemAt(position)
         if item:  # If clicked on an item (node)
             remove_action = QAction("Remove Node", self)
             remove_action.triggered.connect(self.remove_node)
